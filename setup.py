e5c5edfc0ddfcb59d6df6976904908af8d27cea1
Gabriel Falcão <gabriel@nacaolivre.org>
2015-12-13 07:50:05 -0200

    New release: 0.7.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ requirements = [
 
 
 setup(name='steadymark',
-      version='0.7.1',
+      version='0.7.2',
       description=(u'Markdown-based test runner for python. '
                    'Good for github projects'),
       author=u'Gabriel Falcao',
