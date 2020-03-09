6f19428a84aa90745d689a4091e1bc88e17bba3f
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-09 22:01:40 +0100

    fix python versions on pypi

--- setup.py
+++ setup.py
@@ -73,8 +73,8 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
     ],
 
